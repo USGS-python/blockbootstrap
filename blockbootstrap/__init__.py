ccd43ead8c3cbb675b27a570884f3b2b8742c975
thodson-usgs <thodson@usgs.gov>
2019-06-11 16:48:27 -0500

    Assert timeseries is a pandas Series.

--- blockbootstrap/__init__.py
+++ blockbootstrap/__init__.py
@@ -17,9 +17,10 @@ class BBS:
         freq : {'D','H','M','S'}
             Frequency. For example D for days
         """
+        assert isinstance(timeseries, pd.Series), ValueError('timeseries must be a pandas Series.')
         self.ts = timeseries.copy()
 
-        assert type(block_length) is int, ValueError('block_length must be int')
+        assert isinstance(block_length, int), ValueError('block_length must be int')
         self.block_length = pd.Timedelta(f'{block_length} {freq}')
 
         # prep timeseries
@@ -31,8 +32,8 @@ class BBS:
         self.N = self.ts.count()
         self.N_ts = self.ts_range.shape[0]
 
-    def sample(self):
-        """ Sample self.ts with replacement until the lenght of the bootstrap sample
+    def sample(self, seed=None):
+        """ Sample self.ts with replacement until the length of the bootstrap sample
         is equal to the length of self.ts
 
         Returns
@@ -42,8 +43,10 @@ class BBS:
         N_r = 0
         values = np.empty(self.N)
         times = np.empty(self.N, dtype='<M8[ns]')
+
         while N_r < self.N:
             # establish the time window
+            np.random.seed(seed)
             i = np.random.randint(0, self.N_ts)
             start_time = self.ts_range[i]
             end_time = start_time + self.block_length
